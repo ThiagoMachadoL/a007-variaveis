38920a2ccb826b5767c771a7b527e2840622b714
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-07-18 22:41:24 -0300

    exercicio-concluido

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+const temCarro = true
+
+const estudaJs = true
+
+const sabeItaliano = false
+
+const perguntaA = "Você tem carro?";
+const respostaA = prompt(perguntaA);
+console.log(perguntaA,respostaA);
+
+const perguntaB = "Você estuda Javascript?";
+const respostaB = prompt(perguntaB);
+console.log(perguntaB,respostaB);
+
+const perguntaC = "Você fala italiano?"
+const respostaC = prompt(perguntaC);
+console.log(perguntaC,respostaC);
